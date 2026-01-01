877cfce4200f90465ee4ff1c707a7cbc23d7d411
bgagnon93 <bgagnon93@gmail.com>
2026-01-01 15:37:22 -0500

    chore: migrate to vite

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  server: {
+    port: 3000,
+    open: true
+  },
+  build: {
+    outDir: 'build'
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.js$/,
+    exclude: []
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx'
+      }
+    }
+  }
+})
